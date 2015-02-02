c396e4d851422f15891550ce62917c6ce1f16c00
Sean Coyle <scoyle98@gmail.com>
2015-02-02 14:42:50 -0500

    buyers.rb

--- models/buyers.rb
+++ models/buyers.rb
@@ -1,5 +1,8 @@
-require 'activerecord'
+require 'active_record'
 
-class Buyers << ActiveRecord::Base
-
+class Buyers <ActiveRecord::Base
+  has_many: purchases
+  def purchases
+    Purchase.where({buyers_id: self.id})
+  end
 end
