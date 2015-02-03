0f01eb8e608d794a2a3cd7cb533fc394a2bc3ed7
gabrielaldana87 <gabrielaldana87@gmail.com>
2015-02-03 17:54:03 -0500

    creates the index.erb file and buyers.rb file

--- models/buyers.rb
+++ models/buyers.rb
@@ -1,14 +1,5 @@
 require 'active_record'
 
-<<<<<<< HEAD
 class Buyers <ActiveRecord::Base
   has_many :purchases
-  # def purchases
-  #   Purchase.where({buyers_id: self.id})
-  # end
-=======
-class Buyer < ActiveRecord::Base
-	has_many: Purchases
->>>>>>> 10ad7afce4e0c27966c82efbbfdda6dbb4852491
 end
-
