8b67bf4e584bc294519b44181a3dba3af2c65ddd
gabrielaldana87 <gabrielaldana87@gmail.com>
2015-02-04 11:39:37 -0500

    updates the form to link to the purchase method post

---
+++ server.rb
@@ -0,0 +1,61 @@
+require 'sinatra'
+require 'sqlite3'
+require 'sinatra/reloader'
+require_relative './models/connection'
+require_relative './models/buyers'
+require_relative './models/t_name'
+require_relative './models/t_type'
+require_relative './models/connection'
+require 'pry'
+
+after do
+	ActiveRecord::Base.connection.close
+end
+
+get '/shirts' do
+	shirts = Tname.all()
+	shirts = Tname.where({available: true})
+	types =  Ttype.all()
+	colors = Ttype.uniq.pluck(:color)
+	sizes = Ttype.uniq.pluck(:size)
+	genders = Ttype.uniq.pluck(:gender)
+
+	erb(:index, { locals: { shirts: shirts,
+													types: types,
+													colors: colors,
+													sizes: sizes,
+													genders: genders}})
+end
+
+get '/shirt/:id' do
+
+end
+
+get '/add_to_cart' do
+
+end
+
+post '/shirt' do
+	shirt = {
+		name: params["name"],
+		price: params["price"]
+	}
+	Tname.create(shirt)
+	msg = {
+		status: "success"
+	}
+	msg.to_json
+end
+
+delete '/shirt/:id' do
+	id = params[:id]
+	shirts = Ttype.where({tname_id: id})
+	shirts.each do |shirt|
+		Ttypes.destroy(shirt.id)
+	end
+	Tname.destroy({id: id})
+	msg = {
+		status: "success"
+	}
+	msg.to_json
+end
